f26e9122b1abe617bb724e910f6aef513a2ad3c2
JKV21 <vtechgen21@gmail.com>
2023-07-04 12:42:56 +0200

    ...

--- components/ProjectFrom.tsx
+++ components/ProjectFrom.tsx
@@ -131,7 +131,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
         type="url"
         title="Website URL"
         state={form.liveSiteUrl}
-        placeholder="https://jsmastery.pro"
+        placeholder="https://example.com"
         setState={(value) => handleStateChange("liveSiteUrl", value)}
       />
 
@@ -139,7 +139,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
         type="url"
         title="GitHub URL"
         state={form.githubUrl}
-        placeholder="https://github.com/adrianhajdin"
+        placeholder="https://github.com/example"
         setState={(value) => handleStateChange("githubUrl", value)}
       />
 
